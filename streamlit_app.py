b5c40c7d7f541057d1d3333e0856cd92e554e3a3
goshopin <goshopin2024@gmail.com>
2025-04-25 10:42:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,64 +5,95 @@ import requests
 
 # To run this module use : streamlit run filename.py
 
+st.set_page_config(layout='wide')
+
+col1, col2, col3 = st.columns([2,7,1])
 # Set page title
-st.title("GOASSIST, Enter your query ")
 
-# Initialize session state for chat history if it doesn't exist
-if "messages" not in st.session_state:
-    st.session_state.messages = []
+with col1:
 
-# Display chat history
-for message in st.session_state.messages:
-    role = message["role"]
-    content = message["content"]
-    with st.chat_message(role):
-        st.markdown(content)
-
-# OpenAI API key input
-#api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
-#if api_key:
-#    os.environ["OPENAI_API_KEY"] = api_key
-
-# User input
-user_input = st.chat_input("Ask something...")
-
-if user_input: # and api_key:
-    # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": user_input})
+    st.radio('Select the type of assistant you want to operate with',
+             key='assistant type',
+             options = ['chat', 'qna', 'RAG', 'search']
+             )
     
-    # Display user message
-    with st.chat_message("user"):
-        st.markdown(user_input)
-    
-    # Display assistant thinking indicator
-    with st.chat_message("assistant"):
-        message_placeholder = st.empty()
-        message_placeholder.markdown("Thinking...")
+    model_option = st.selectbox('Select the Model',
+                         ('gpt2',
+                        'gpt4',
+                        'llama2',
+                        'llama4',
+                         ),
+                        placeholder='gpt2')
+    st.write('Your selected model : ', model_option)
+
+    task_option = st.selectbox('What do you want to do ?',
+                         ('Praphrase',
+                         'Summary',
+                         'RAG',
+                         'Blog'))
+    st.write('Your selected action : ', task_option)
+
+
+with col2:
+    st.title("GOASSIST")
+
+    # Initialize session state for chat history if it doesn't exist
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    # Display chat history
+    for message in st.session_state.messages:
+        role = message["role"]
+        content = message["content"]
+        with st.chat_message(role):
+            st.markdown(content)
+
+    # OpenAI API key input
+    #api_key = st.sidebar.text_input("Enter your OpenAI API key:", type="password")
+    #if api_key:
+    #    os.environ["OPENAI_API_KEY"] = api_key
+
+    # User input
+    user_input = st.chat_input("Ask something...")
+
+    if user_input: # and api_key:
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": user_input})
         
-        # Initialize LLM
-        # llm = ChatOpenAI(model="gpt-3.5-turbo")
+        # Display user message
+        with st.chat_message("user"):
+            st.markdown(user_input)
         
-        # Get response from LLM
-        try:
-            #response = llm.invoke(user_input) # Call GPT function here
-            data = {'prompt': user_input} 
+        # Display assistant thinking indicator
+        with st.chat_message("assistant"):
+            message_placeholder = st.empty()
+            message_placeholder.markdown("Thinking...")
             
-            url = 'http://localhost:8000/chatapp/chat'
-            response = requests.get(url,
-                             params=data)
+            # Initialize LLM
+            # llm = ChatOpenAI(model="gpt-3.5-turbo")
             
-            print('llm response', response.text)
+            # Get response from LLM
+            try:
+
             
-            assistant_response = response.text # response.content
-            
-            
-            # Update assistant message
-            message_placeholder.markdown(assistant_response)
-            
-            # Add assistant response to chat history
-            st.session_state.messages.append({"role": "assistant", "content": assistant_response})
-        except Exception as e:
-            message_placeholder.markdown(f"Error: {str(e)}")
-elif user_input:
-    st.warning("Please enter your OpenAI API key in the sidebar.")
+                #response = llm.invoke(user_input) # Call GPT function here
+                data = {'prompt': user_input, 'stream': True, 'model_name' : model_option} 
+                
+                url = 'http://localhost:8000/chatapp/chat'
+                response = requests.get(url,
+                                params=data)
+                
+                print('llm response', response.text)
+                
+                assistant_response = response.text # response.content
+                
+                # Update assistant message
+                message_placeholder.markdown(assistant_response)
+                
+                # Add assistant response to chat history
+                st.session_state.messages.append({"role": "assistant", "content": assistant_response})
+            except Exception as e:
+                message_placeholder.markdown(f"Error: {str(e)}")
+    elif user_input:
+        st.warning("Please enter your OpenAI API key in the sidebar.")
+
